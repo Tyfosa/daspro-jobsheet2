e1fad9ec61b63a866f216d3653992af5fbb8561c
Tyfosa <107738043+Tyfosa@users.noreply.github.com>
2024-09-05 14:16:43 +0700

    BelajatJava
    30 agustus 2024

---
+++ MyFirstJava23.java
@@ -0,0 +1,19 @@
+public class MyFirstJava23 {
+
+    public static void main(String[] args) {
+
+        System.out.println("--------------------------------------------------------");  
+        System.out.println("-- DATA MAHASISWA JURUSAN TEKNOLOGI INFORMASI POLINEMA--");  
+        System.out.println("--------------------------------------------------------");   
+        System.out.println("Nama       : Tyfosa Helga Wijaya");  
+        System.out.println("NIM        : 244107060071");  
+        System.out.println("Kelas      : SIB 1A");  
+        System.out.println("No Absen   : 23");  
+        System.out.println("Prodi      : SISTEM INFORMASI BISNIS");  
+        System.out.println("Alamat     : jl. papa hijau 2");  
+        System.out.println("No HP      : 08113377009");  
+        System.out.println("Hobi       : TIDUR");
+
+    }
+    
+}
