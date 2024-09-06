5dd1a90928cb5cefc4a2932ea66e08f63ebf7a79
tersiqo <118157349+tersiqo@users.noreply.github.com>
2024-09-06 14:04:37 +0700

    Update belajarcollabgithub.java

--- belajarcollabgithub.java
+++ belajarcollabgithub.java
@@ -1,9 +1,18 @@
 public class belajarcollabgithub {
     
     public static void main(String[] args) {
-        System.out.println("ini kelompok kita");
-        System.out.println("ini kelompok kita");
-        System.out.println("ini kelompok kita");
+        System.out.println("Nama   : Tersiqo Alfarezel");
+        System.out.println("NIM    : 244107060089");
+        System.out.println("Alamat : Perum ARUMBA HILL RESIDENCE KAV 12");
+        System.out.println("-----------------------------------------------");
+        System.out.println("Nama   : Tersiqo Alfarezel");
+        System.out.println("NIM    : 244107060089");
+        System.out.println("Alamat : Perum ARUMBA HILL RESIDENCE KAV 12");
+        System.out.println("Nama   : Tersiqo Alfarezel");
+        System.out.println("NIM    : 244107060089");
+        System.out.println("Alamat : Perum ARUMBA HILL RESIDENCE KAV 12");
+        
+        
 
     }
 }
