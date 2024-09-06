fa4af789355a216420aba108583cf52f31cde423
tersiqo <118157349+tersiqo@users.noreply.github.com>
2024-09-06 14:00:12 +0700

    Update belajarcollabgithub.java

--- belajarcollabgithub.java
+++ belajarcollabgithub.java
@@ -3,6 +3,7 @@ public class belajarcollabgithub {
     public static void main(String[] args) {
         System.out.println("ini kelompok kita");
         System.out.println("ini kelompok kita");
+        System.out.println("ini kelompok kita");
 
     }
 }
